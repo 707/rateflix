496aadf82af38602be0d5df501bfd81e07f14870
Nadeem Shaik <venomreborn@gmail.com>
2016-02-15 23:39:55 +0530

    Added Openshift config

--- src/app.js
+++ src/app.js
@@ -22,7 +22,14 @@ var content;
 var content2
 var app = express();
 
-
+// OpenShift Configuration
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
+//
 app.set('view engine', 'jade'); //set view engine
 app.set('views', __dirname + '/views'); //set view to folder path 
 
